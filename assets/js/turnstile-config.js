87c68460a6a87d068cd5b3b0ae1dee08e8b1f824
LichiGMDJ <aleksandrrubacev222@gmail.com>
2026-08-26 23:19:22 +0500

    feat: add production Turnstile config

---
+++ assets/js/turnstile-config.js
@@ -0,0 +1,6 @@
+window.NIGHT_TURNSTILE_CONFIG = Object.freeze({
+    siteKey: '0x4AAAAAAEdDosVBYucj1XEx',
+    workerBaseUrl: 'https://night-gdps-access-gate.aleksandrrubacev222.workers.dev',
+    sessionStorageKey: 'night_gdps_access_v1',
+    sessionHours: 8
+});
